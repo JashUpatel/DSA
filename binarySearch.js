14255f4e9d34b41cca40ee986b56b92a3b4bebd6
JashUpatel <pateljash77@gmail.com>
2025-05-21 18:46:42 +0530

    binary search using recursion

--- binarySearch.js
+++ binarySearch.js
@@ -23,4 +23,31 @@ const binarySearchMethod = (nums, target) => {
   return -1;
 };
 
+const binarySearchRecursiveFunc = (nums, start, end, target) => {
+  // base condition for breaking/termination
+  if (start > end) {
+    return -1;
+  }
+
+  let mid = parseInt((start + end) / 2);
+
+  // recursive conditions
+  if (nums[mid] == target) {
+    return mid;
+  } else if (nums[mid] < target) {
+    return binarySearchRecursiveFunc(nums, mid + 1, end, target);
+  } else {
+    return binarySearchRecursiveFunc(nums, start, mid - 1, target);
+  }
+};
+
+const binarySearchMethodUsingRecursion = (nums, target) => {
+  let start = 0;
+  let end = nums.length - 1;
+
+  const ans = binarySearchRecursiveFunc(nums, start, end, target);
+  return ans;
+};
+
 console.log(binarySearchMethod(nums, 2));
+console.log(binarySearchMethodUsingRecursion(nums, 2));
